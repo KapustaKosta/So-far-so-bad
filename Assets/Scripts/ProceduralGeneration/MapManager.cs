7bc61f6498de136e014d3e778395ca343b6ad037
Maxim <73167493+m4xxx1m@users.noreply.github.com>
2021-12-25 09:18:29 +0400

    динамическая сложность

--- Assets/Scripts/ProceduralGeneration/MapManager.cs
+++ Assets/Scripts/ProceduralGeneration/MapManager.cs
@@ -72,6 +72,11 @@ public class MapManager : MonoBehaviour
                 RemoveMap(chunkNumArray[0]);
                 chunkNumArray[0] = chunkNumArray[1];
                 chunkNumArray[1] = chunkNumArray[1] + 1;
+                itemsCount[0] += 1;
+                if (caveType == CaveType.Vrudni)
+                {
+                    itemsCount[3] += (chunkNumArray[1] % 2 == 0 ? 1 : 0);
+                }
                 GenerateMap(chunkNumArray[1]);
                 Points.CurrentChunk = chunkNumArray[1];
             }
